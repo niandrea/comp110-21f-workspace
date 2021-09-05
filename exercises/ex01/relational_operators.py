eff5758dcadc189b41217c2d6e4b77323d2639c4
Andrea Rasco <niandrea@email.unc.edu>
2021-09-05 17:53:26 -0400

    Lesson works

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,6 +1,11 @@
 """COMP 110 Exercise 01, Relational Operators."""
 __author__ = "730396438"
 
+
+int(1.0) + 2 / int("2")
+
+
+
 x: str = input("Left-hand side: ")
 y: str = input("Right-hand side: ")
 
